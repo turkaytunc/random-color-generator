c435384290a6125b8fad8fc03a0929d9e80d2e01
Turkay Tunc <turkaytunc00@gmail.com>
2020-09-15 18:39:48 +0300

    Updates

---
+++ precache-manifest.38faafcb0755e44958741f409bf28f4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d9e3eb5985497fd7a99960f14ef29ac",
+    "url": "/random-color-generator/index.html"
+  },
+  {
+    "revision": "7ddc90dd9db2ee68ffe4",
+    "url": "/random-color-generator/static/css/main.9d477211.chunk.css"
+  },
+  {
+    "revision": "c739ba9cfe07fe53766e",
+    "url": "/random-color-generator/static/js/2.46f88f2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-color-generator/static/js/2.46f88f2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ddc90dd9db2ee68ffe4",
+    "url": "/random-color-generator/static/js/main.45eee939.chunk.js"
+  },
+  {
+    "revision": "e873541e1671e35d3cbc",
+    "url": "/random-color-generator/static/js/runtime-main.7306c3ed.js"
+  }
+]);
