727f415c0379be72092f70203d9f0b71f519cd3c
Turkay Tunc <turkaytunc00@gmail.com>
2020-09-15 20:08:34 +0300

    add copy to clipboard button

--- src/components/RandomColor.js
+++ src/components/RandomColor.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { generateNumber } from '../util/generate-number';
 import './random-color.scss';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 export const RandomColor = () => {
   const [colorValue, setColorValue] = useState({
@@ -8,6 +9,7 @@ export const RandomColor = () => {
     green: 200,
     blue: 200,
   });
+  const clipboardRef = useRef(null);
 
   const generateColorValue = () => {
     const redColorValue = generateNumber();
@@ -22,12 +24,24 @@ export const RandomColor = () => {
     });
   };
 
+  const copyToClipboard = (e) => {
+    clipboardRef.current.select();
+    document.execCommand('copy');
+  };
+
   const color = `rgb(${colorValue.red}, ${colorValue.green}, ${colorValue.blue})`;
   return (
     <div className="card-wrapper">
       <div style={{ background: color, height: '20em' }}></div>
       <div>
-        <p className="color-code">{color}</p>
+        <p ref={clipboardRef} className="color-code">
+          {color}
+        </p>
+        <button>
+          <CopyToClipboard text={color}>
+            <span>Copy to clipboard</span>
+          </CopyToClipboard>
+        </button>
       </div>
       <button
         onClick={() => {
